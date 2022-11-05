0fd4a0bcb355b8c171ddb4d113d1a69758bf42d8
郑嘉俊 <2426091982@qq.com>
2022-11-05 22:34:19 +0800

    socket 设置

--- server/app.js
+++ server/app.js
@@ -1,8 +1,23 @@
 const Koa = require('koa');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const app = new Koa();
 
+const httpServer = createServer(app.callback());
+const io = new Server(httpServer, { /* options */ });
+
+// socket 链接
+io.on("connection", (socket) => {
+  console.log(socket.id)
+  // 链接成功发送 returnId 事件
+  socket.emit("returnId",socket.id );
+});
+
+// 中间件
 app.use(async ctx => {
   ctx.body = 'koa run to port 3000';
 });
 
-app.listen(3000);
+app.listen(3000,()=>{
+    console.log('服务已经在 http://localhost:3000 端口开启')
+});
